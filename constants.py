7c1b9b5ff5e872645a6c511ea2bb50cca58b35b4
jawlecks <alex@alexjolly.me>
2018-04-26 15:13:39 -0400

    Remove unnecessary URL

--- constants.py
+++ constants.py
@@ -4,6 +4,5 @@ MESSAGE_TEXT = '%s is live with %s, tune in now! %s'
 EMBED_TYPE = 'rich'
 EMBED_COLOR = 6570404
 FOOTER_TEXT = 'Stream started'
-FOOTER_ICON_URL = 'https://cdn.discordapp.com/avatars/438788643318923274/414cb072317da888661928fd3b5cce5a.png'
 AUTHOR_TEXT = '%s is now streaming!'
 AUTHOR_ICON_URL = 'https://cdn.discordapp.com/emojis/287637883022737418.png'
