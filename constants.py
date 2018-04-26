c41b424f5d9c52e199d8b4b6529cd10662d648d1
jawlecks <alex@alexjolly.me>
2018-04-26 15:15:30 -0400

    Add credit

--- constants.py
+++ constants.py
@@ -3,6 +3,6 @@ from secrets import *
 MESSAGE_TEXT = '%s is live with %s, tune in now! %s'
 EMBED_TYPE = 'rich'
 EMBED_COLOR = 6570404
-FOOTER_TEXT = 'Stream started'
+FOOTER_TEXT = 'Bot created by @jawlecks | Stream started'
 AUTHOR_TEXT = '%s is now streaming!'
 AUTHOR_ICON_URL = 'https://cdn.discordapp.com/emojis/287637883022737418.png'
